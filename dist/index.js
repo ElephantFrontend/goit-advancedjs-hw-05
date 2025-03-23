4ef62f4fea94b5c360b36db0f0818d3d6a296517
elephant <elephantfrontend@gmail.com>
2025-03-23 22:14:10 +0200

    made task

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+function getName(name) {
+    return name;
+}
+getName('Illia');
+console.log('Test file');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function getName(name: string) {
+    return name;
+}
+getName('Illia');
+
+console.log('Test file');
